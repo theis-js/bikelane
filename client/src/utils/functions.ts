be7d49820d454ebdbe2435b667eea1dc223cb0e4
theis.gaedigk <theis.gaedigk@icloud.com>
2025-07-22 21:14:01 +0200

    fix: ensure proper cleanup during logout by removing token and users from storage

--- client/src/utils/functions.ts
+++ client/src/utils/functions.ts
@@ -6,5 +6,7 @@ export const greeting = () => {
 
 export const logout = () => {
   Cookies.remove("name");
+    Cookies.remove("token");
+  localStorage.removeItem("users");
   window.location.reload();
 };
