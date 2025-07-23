a7ee03bce0b62c5d75d003aa7a226098213ee8eb
theis.gaedigk <theis.gaedigk@icloud.com>
2025-07-23 14:57:40 +0200

    updated scheme

--- scheme.sql
+++ scheme.sql
@@ -6,7 +6,8 @@ CREATE TABLE users (
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    created TIMESTAMP NOT NULL DEFAULT NOW()
+    created TIMESTAMP NOT NULL DEFAULT NOW(),
+    role VARCHAR(50) NOT NULL DEFAULT "user"
 );
 
 -- Mock data for users
